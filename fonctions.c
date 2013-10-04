9bb3dd353c57beac4725a7b15fd7ccec7df989f0
poupou9779 <robinpetit1997@hotmail.com>
2013-10-04 17:45:47 +0200

    Create fonctions.c
    File which contains the functions for the tries of main.c

---
+++ fonctions.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "fonctions.h"
+
+void riri(void) { printf("Riri\n"); }
+void fifi(void) { printf("Fifi\n"); }
+void loulou(void) { printf("Loulou\n"); }
+void donald(void) { printf("Donald\n"); }
+void picsou(void) { printf("Picsou\n"); }
