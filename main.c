5ac0379ee103814c53f409b765e34c94f40334ed
Robin Petit <robinpetit1997@hotmail.com>
2013-10-08 21:47:02 +0200

    Update main.c
    Adapted to the new errors.

--- main.c
+++ main.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Menu.h"
 #include "fonctions.h"
 
-int main(void) {
-    int n = 0;
+int main(int argc, char *argv[]) {
+    int n = 0, ret;
     struct Menu *menu;
     Menu_initialiser();
+    atexit(Menu_Quitter);
     menu = Menu_allouerMenu();
     if(menu != NULL) {
         if(Menu_ajouterFonction(menu, riri, "riri") != MENU_SUCCESS)
-            Menu_erreur(stdout);
+            printf("Ajouter riri : %s\n", Menu_GetErreur());
         if(Menu_ajouterFonction(menu, fifi, "fifi") != MENU_SUCCESS)
-            Menu_erreur(stdout);
+            printf("Ajouter fifi : %s\n", Menu_GetErreur());
         if(Menu_ajouterFonction(menu, loulou, "loulou") != MENU_SUCCESS)
-            Menu_erreur(stdout);
+            printf("Ajouter loulou : %s\n", Menu_GetErreur());
         if(Menu_afficherMenu(menu) != MENU_SUCCESS)
-            Menu_erreur(stdout);
+            printf("Afficher menu : %s\n", Menu_GetErreur());
         scanf("%d", &n);
-        if(Menu_executerFonction(menu, n) != MENU_SUCCESS)
-            Menu_erreur(stdout);
+        if(Menu_executerFonction(menu, n, argc, argv, &ret) != MENU_SUCCESS)
+            printf("Executer fonction : %s\n", Menu_GetErreur());
+        else
+            printf("retourne : %d", ret);
         menu = Menu_desallouerMenu(menu);
     }
-    Menu_Quitter();
     return 0;
 }
-
-
