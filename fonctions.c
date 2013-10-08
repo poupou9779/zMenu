a2e096e07616a1a51d433cc9c896ec8257b729f4
Robin Petit <robinpetit1997@hotmail.com>
2013-10-08 21:46:19 +0200

    Update fonctions.c
    Adapted the test functions to the new callback type (int (int, char *[])).

--- fonctions.c
+++ fonctions.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "fonctions.h"
 
-void riri(void) { printf("Riri\n"); }
-void fifi(void) { printf("Fifi\n"); }
-void loulou(void) { printf("Loulou\n"); }
-void donald(void) { printf("Donald\n"); }
-void picsou(void) { printf("Picsou\n"); }
+int riri(int argc, char *argv[]) { printf("Riri\n"); return 1; (void)argc; (void)argv; }
+int fifi(int argc, char *argv[]) { printf("Fifi\n"); return 2; (void)argc; (void)argv;}
+int loulou(int argc, char *argv[]) { printf("Loulou\n"); return 3; (void)argc; (void)argv;}
+int donald(int argc, char *argv[]) { printf("Donald\n"); return 4; (void)argc; (void)argv;}
+int picsou(int argc, char *argv[]) { printf("Picsou\n"); return 5; (void)argc; (void)argv;}
