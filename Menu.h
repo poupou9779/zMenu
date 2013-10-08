917f596b979c0a972b1d8673260874807e6f61cf
Robin Petit <robinpetit1997@hotmail.com>
2013-10-08 21:45:35 +0200

    Update Menu.h
    Added comm's. Simplified errors.

--- Menu.h
+++ Menu.h
@@ -1,26 +1,17 @@
 #ifndef MENU_H
 #define MENU_H
 
-#define Menu_erreur(a) Menu_GetErreur(a, __LINE__, __FILE__)
-
-#define MENU_SUCCESS                 0x0000
-#define MENU_NULL_POINTER_PARAMETER -0x0001
-#define MENU_OVERFLOW_CALLBACK      -0x0002
+#define MENU_SUCCESS 0x0000
 
 #define NOMBRE_MAX 20
 #define LONGUEUR 100
 
-enum {MENU_AJOUTER_FONCTION, MENU_EXECUTER, MENU_AFFICHER};
+typedef int (*callback)(int , char *[]);
 
-typedef void (*callback)(void);
+char *Menu_Erreur;
 
-struct Menu_Erreur {
-    int erreur;
-    int fonction;
-    //si fonction == API_AJOUTER_FONCTION
-    char nom[LONGUEUR];
-};
-struct Menu_Erreur menu_erreur;
+/**Buffer temporaire pour mettre le message d'erreur dans Menu_Erreur*/
+char Menu_TMPerreur[128];
 
 struct Menu_Fonction {
     callback ptr;
@@ -28,17 +19,41 @@ struct Menu_Fonction {
 };
 
 struct Menu {
-    unsigned int nombre;
+    int nombre;
     struct Menu_Fonction fonctions[NOMBRE_MAX];
 };
 
-void            Menu_initialiser(void);
-void            Menu_GetErreur(FILE *, int, char *);
-struct Menu *   Menu_allouerMenu(void);
-int             Menu_ajouterFonction(struct Menu *, callback, char *);
-int             Menu_afficherMenu(struct Menu *);
-struct Menu *   Menu_desallouerMenu(struct Menu *);
-int             Menu_executerFonction(struct Menu *, int);
-void            Menu_Quitter(void);
+/**Modifie Menu_Erreur en "Fonction : <fonction>\n\tErreur : <erreur>\n"*/
+void Menu_actuErreur(const char *fonction, const char *erreur);
+
+/**Actuellement, ne fait rien. Pourrait servir aux éventuelles allocations dynamiques, ...*/
+void Menu_initialiser(void);
+
+/**Revoie un pointeur constant sur Menu_Erreur.
+    Renvoie NULL si Menu_actuErreur a échoué*/
+const char *Menu_GetErreur(void);
+
+/**Renvoie un pointeur sur un menu alloué.
+    Renvoie NULL si l'allocation a échoué*/
+struct Menu *Menu_allouerMenu(void);
+
+/**Ajoute la fonction "fonction" dans le menu "menu", et nomme cette fonction "nom".
+    Renvoie MENU_SUCCESS si nom, fonction et menu ne sont pas NULL*/
+int Menu_ajouterFonction(struct Menu *menu, callback fonction, char *nom);
+
+/**Liste tous les éléments du menu passé en paramètre.
+    Renvoie MENU_SUCCESS si menu n'est pas NULL*/
+int Menu_afficherMenu(struct Menu *menu);
+
+/**Libère la mémoire allouée pour le menu.
+    Renvoie NULL*/
+struct Menu *Menu_desallouerMenu(struct Menu *menu);
+
+/**Appelle la fonction n° index dans menu. Envoie argc et argv en paramètres, et récupère le résultat dans ret.
+    Renvoie MENU_SUCCESS en cas de réussite*/
+int Menu_executerFonction(struct Menu *menu, int index, int argc, char *argv[], int *ret);
+
+/**Libère Menu_Erreur*/
+void Menu_Quitter(void);
 
 #endif // MENU_H
